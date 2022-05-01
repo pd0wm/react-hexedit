9b661ffbb2fa90275a3615370981f17576f740ef
Willem Melching <willem.melching@gmail.com>
2022-05-01 14:37:25 +0200

    cleanup and first component

---
+++ src/components/HexEditorRow.js
@@ -0,0 +1,13 @@
+import { Component } from "react"
+
+class HexEditorRow extends Component {
+  render() {
+    return (
+      <div>
+        I'm a row
+      </div>
+    );
+  }
+}
+
+export default HexEditorRow;
